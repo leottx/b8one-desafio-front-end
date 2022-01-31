bcfcc80869adfdaf8f7fe5c6a83312606c3cdd61
leotx <leotx@pop-os.localdomain>
2022-01-31 17:11:59 -0300

    build(boilerplate): cria o boilerplate do projeto

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+
+// Plugin para otimizar imagens
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+// Plugin para minificar os arquivos CSS e extrai-los para um diretorio particular
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'assets/styles/[name].[contenthash].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.squooshMinify,
+          options: {
+            encodeOptions: {
+              mozjpeg: {
+                quality: 50,
+              },
+              pngquant: {
+                quality: [0.5, 0.5],
+              },
+            },
+          },
+        },
+      }),
+    ],
+  },
+});
